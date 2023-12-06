9343088bd2f9e0bf3ef7ad3222b05661a4660226
LarsSong1 <damianalejandrofirst@gmail.com>
2023-12-05 21:43:29 -0500

    10  commit

--- barberia/settings.py
+++ barberia/settings.py
@@ -134,14 +134,13 @@ STATIC_URL = '/static/'
 
 
 
-# if not DEBUG:
+if not DEBUG:
 
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # # Default primary key field type
 # # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
