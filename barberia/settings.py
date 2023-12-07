468d4c002a4ec18e7cb8e3b4262f67ca09989692
LarsSong1 <damianalejandrofirst@gmail.com>
2023-12-06 21:09:46 -0500

    14  commit

--- barberia/settings.py
+++ barberia/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-70wgj^)vema138te*1p&arusnp1qdcfjqp3k4*o2y@h%6#b(4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -85,19 +86,35 @@ WSGI_APPLICATION = 'barberia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # DATABASES = {
-#     "default": dj_database_url.config(os.environ.get('DATABASE_URL'))
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "postgres",
+#         "USER": "root",
+#         "PASSWORD": "C9atUcRa1JuyxDqg",
+#         "HOST": "postgresql-rds-production-itchy-apple-41xe.cguuimxinmgi.us-west-2.rds.amazonaws.com",
+#         "PORT": "5432",
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
+
+DATABASE_URL = 'postgres://root:C9atUcRa1JuyxDqg@postgresql-rds-production-itchy-apple-41xe.cguuimxinmgi.us-west-2.rds.amazonaws.com:5432/postgres'
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
